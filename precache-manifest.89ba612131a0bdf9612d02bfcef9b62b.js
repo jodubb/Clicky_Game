72c74cce56ecf2c720378d92a5e9036c9baf42a7
Mona Lisa <jodubb913@gmail.com>
2019-06-25 20:55:12 -0400

    Updates

---
+++ precache-manifest.89ba612131a0bdf9612d02bfcef9b62b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1459258623207cf95db9fd9c88342733",
+    "url": "/Clicky_Game/index.html"
+  },
+  {
+    "revision": "dc1c38f9efb43859e67c",
+    "url": "/Clicky_Game/static/css/main.b35e3f08.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Clicky_Game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "dc1c38f9efb43859e67c",
+    "url": "/Clicky_Game/static/js/main.0048807e.chunk.js"
+  },
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  },
+  {
+    "revision": "fb9c85aa72b727f675ad83933e7c489e",
+    "url": "/Clicky_Game/static/media/90scartoons2.fb9c85aa.jpg"
+  },
+  {
+    "revision": "5f3c35d78ad09bd32cc4ab3e073fcff3",
+    "url": "/Clicky_Game/static/media/Simpsons.5f3c35d7.jpg"
+  },
+  {
+    "revision": "052a3be1c2a47111fc779cc6b8cad8d9",
+    "url": "/Clicky_Game/static/media/beavus.052a3be1.jpg"
+  },
+  {
+    "revision": "6f696c5f16235e10adeaf94e295fb902",
+    "url": "/Clicky_Game/static/media/couragedog.6f696c5f.jpg"
+  },
+  {
+    "revision": "d996c2c69eece3daa0254cd245b5a542",
+    "url": "/Clicky_Game/static/media/dexter.d996c2c6.jpg"
+  },
+  {
+    "revision": "fc06ccb070cc13326be4c7bb08ddf92c",
+    "url": "/Clicky_Game/static/media/doug.fc06ccb0.jpg"
+  },
+  {
+    "revision": "70202f848702aedc92d2cd16d75a7c38",
+    "url": "/Clicky_Game/static/media/edds2.70202f84.jpg"
+  },
+  {
+    "revision": "2b239d4c0ad00fc9ad4ff359efa0d661",
+    "url": "/Clicky_Game/static/media/flinstones.2b239d4c.jpg"
+  },
+  {
+    "revision": "aa19f2c276d2ed5176473d68ab31aecf",
+    "url": "/Clicky_Game/static/media/hey_arnold.aa19f2c2.jpg"
+  },
+  {
+    "revision": "bb6684ed13209199628cba9d5cdc57e6",
+    "url": "/Clicky_Game/static/media/jetsons.bb6684ed.jpg"
+  },
+  {
+    "revision": "9e16d47f0493400d70a86177315c971b",
+    "url": "/Clicky_Game/static/media/recess2.9e16d47f.jpg"
+  },
+  {
+    "revision": "978d332c1e9f991144b29a4cae629e78",
+    "url": "/Clicky_Game/static/media/rugrats.978d332c.jpg"
+  }
+]);
